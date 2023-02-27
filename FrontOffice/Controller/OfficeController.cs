9ed661cc4a1b895039c8314d9716d8304f499d55
Busystem TD <Busystem TD@LAPTOP-E28UIIVQ>
2023-02-27 09:59:52 +0700

    Add project files.

---
+++ FrontOffice/Controller/OfficeController.cs
@@ -0,0 +1,32 @@
+﻿using FrontOffice.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FrontOffice.NewFolder
+{
+    public class OfficeController : Controller
+    {
+        private readonly ILogger<OfficeController> _logger;
+
+        public OfficeController(ILogger<OfficeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
